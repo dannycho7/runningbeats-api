2899751f1963f66d8a5130f6463a47af42e72664
dannycho7 <dannycho7@gmail.com>
2018-07-28 00:24:23 -0700

    Acess token as class prop in SpotifyWrapper

--- SpotifyWrapper.js
+++ SpotifyWrapper.js
@@ -2,19 +2,26 @@ const request = require("request");
 
 class SpotifyWrapper {
 	constructor(access_token) {
-		this.access_token = access_token;
+		this.access_token = access_token || process.env['DEFAULT_ACCESS_TOKEN'];
 	}
 
-	fetchTracks(access_token) {
+	fetchTracks() {
 		var options = {
 			url: 'https://api.spotify.com/v1/me/tracks',
-			headers: { 'Authorization': `Bearer ${access_token}` },
+			headers: { 'Authorization': `Bearer ${this.access_token}` },
 			json: true
 		};
 
 		const ids = [];
 
 		request.get(options, function(error, response, body) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+
+			console.log(body);
+
 			let tracks = body['items'];
 			tracks.forEach(track_obj => {
 				let track = track_obj['track'];
@@ -27,7 +34,7 @@ class SpotifyWrapper {
 	mapToBPM(track_ids) {
 		let options = {
 			url: 'https://api.spotify.com/v1/audio-features',
-			headers: { 'Authorization': `Bearer ${access_token}` },
+			headers: { 'Authorization': `Bearer ${this.access_token}` },
 			tracks: track_ids.split(","),
 			json: true
 		}
